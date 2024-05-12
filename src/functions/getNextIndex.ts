b422a911515be98dc8ac3f08870287a064de5c86
Soap-0020 <csc2110a@gmail.com>
2024-05-12 10:14:07 +0100

    Fix getNextIndex function

--- src/functions/getNextIndex.ts
+++ src/functions/getNextIndex.ts
@@ -1,5 +1,5 @@
 const getNextIndex = (array: any[], currentIndex: number | null): number => {
-  if (!currentIndex || array.length == currentIndex + 1) return 0;
+  if (currentIndex == null || array.length == currentIndex + 1) return 0;
   return currentIndex + 1;
 };
 
